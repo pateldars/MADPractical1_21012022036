ecb6d32648daeb4234df4ff146fa3db7db7def4d
Denisha2001 <savaliyadenisha143@gmail.com>
2022-08-10 15:17:18 +0530

    Practical3

---
+++ src/main/kotlin/practical3.kt
@@ -0,0 +1,29 @@
+fun main()
+{
+    println("student enrollment no :")
+    val a= readLine()!!.toDouble()
+    println("student name :")
+    val b= readLine()!!
+    println("student branch :")
+    val c= readLine()!!
+    println("student class :")
+    val d= readLine()!!
+    println("student batch :")
+    val e= readLine()!!
+    println("student college name :")
+    val f= readLine()!!
+    println("student university name :")
+    val i= readLine()!!
+    println("student age :")
+    val j= readLine()!!.toInt()
+
+    println("student enrollment no :$a")
+    println("student name :$b")
+    println("student branch :$c")
+    println("student class :$d")
+    println("student batch :$e")
+    println("student college name :$f")
+    println("student university name :$i")
+    println("student age :$j")
+
+}
